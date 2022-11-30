0ae14473cb51ac376dfa649ea22394a90dec7d3a
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-11-30 16:29:36 -0500

    work on username post + get

--- index.js
+++ index.js
@@ -11,10 +11,33 @@ mongoose.connect(URI).then(
   err => { console.log(err) }
 );
 
-app.use(cors())
-app.use(express.static('public'))
+const Schema = mongoose.Schema;
+
+const usernameSchema = new Schema({ 
+  username: { type: String, required: true }
+})
+
+const Username = mongoose.model('Username', usernameSchema);
+
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
+});
+
+app.post('/api/users', async (req, res, next) => {
+  const newUserName = new Username({ username: req.body.username });
+  
+  await newUserName.save((err) => {
+    if(err) return console.error(err);
+  });
+
+  res.json( newUserName );
+});
+
+app.get('/api/users', (req, res, next) => {
+ res.json([req]);
 });
 
 
