b42e2540292d03728d5eed76229edeef41428b83
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-12-01 17:18:37 -0500

    work on new post req

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose');
-const { ObjectID, ObjectId } = require('bson');
 require('dotenv').config()
 
 const URI = process.env.URI;
@@ -18,7 +17,15 @@ const usernameSchema = new Schema({
   username: { type: String, required: true },
 }, { versionKey: false });
 
+const exerciseSchema = new Schema({
+  username: { type: String},
+  description: {type: String, required: true},
+  duration: {type: Number, required: true},
+  date: {type: String}
+},{ versionKey: false });
+
 const Username = mongoose.model('Username', usernameSchema);
+const Exercise = mongoose.model('Exercise', exerciseSchema);
 
 app.use(cors());
 app.use(express.static('public'));
@@ -48,6 +55,39 @@ app.get('/api/users', (req, res, next) => {
  });
 });
 
+//post exercise form data to /api/users/:id/exercises
+app.post('/api/users/:_id/exercises', async (req, res, next) => {
+  const userId = req.body.id;
+  const description = req.body.description;
+  const duration = req.body.duration;
+  const date = req.body.date;
+  const user = await Username.findById(userId, (err, user) => {
+    if(err) {
+      return console.error(err);
+    }
+    if(!user){
+      return console.log("user does not exist, please create a user first");
+    }
+    return user;
+  });
+  const newExercise = await new Exercise({
+    username: user,
+    description: description,
+    duration: duration,
+    date: date,
+    _id: userId
+  });
+  newExercise.save((err, exercise) => {
+    if(err) {
+      return console.error(err);
+    }
+    res.json(exercise);
+  });
+  
+
+
+});
+
 const listener = app.listen(process.env.PORT || 3001, () => {
   console.log('Your app is listening on port ' + listener.address().port)
-})
+});
